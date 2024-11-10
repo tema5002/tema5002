f8d499940aeb600753523fc60d9b8b47330ee9ac
tema5002 <152618947+tema5002@users.noreply.github.com>
2024-11-10 11:52:04 +0300

    Add files via upload

---
+++ 2dgraphics.hpp
@@ -0,0 +1,106 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+
+struct RGBColor {
+    uint8_t r = 255;
+    uint8_t g = 255;
+    uint8_t b = 255;
+};
+
+class Window {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+public:
+    Window(const char* name, const int w, const int h) {
+        if (SDL_Init(SDL_INIT_EVERYTHING) < 0 ||
+            !((window = SDL_CreateWindow(name, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, w, h, SDL_WINDOW_SHOWN))) ||
+            !((renderer = SDL_CreateRenderer(window, -1, 0)))
+        ) exit(1);
+    }
+
+    void scale(const float x, const float y) const {
+        SDL_RenderSetScale(renderer, x, y);
+    }
+
+    void close() const {
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        exit(0);
+    }
+
+    void rect(const int x, const int y, const int w, const int h, const RGBColor c) const {
+        SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, 255);
+        SDL_Rect rect_ = {x, y, w, h};
+        SDL_RenderFillRect(renderer, &rect_);
+    }
+
+    void pixel(const int x, const int y, const RGBColor c) const {
+        SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, 255);
+        SDL_RenderDrawPoint(renderer, x, y);
+    }
+
+    void circle(const int x, const int y, const int radius, const RGBColor c) const {
+        for (int i = 0; i < radius*2; i++) {
+            for (int j = 0; j < radius*2; j++) {
+                int dx = i - radius;
+                int dy = j - radius;
+                int distance = dx * dx + dy * dy;
+
+                if ((radius - 1) * (radius - 1) <= distance && distance <= radius * radius) {
+                    pixel(x - radius + i, y - radius + j, c);
+                }
+            }
+        }
+    }
+
+    void line(const int x1, const int y1, const int x2, const int y2, const RGBColor c) const {
+        SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, 255);
+        SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+    }
+
+    void rect_outline(const int x1, const int y1, const int w, const int h, const RGBColor c) const {
+        line(x1,     y1,     x1 + w, y1,     c);
+        line(x1 + w, y1,     x1 + w, y1 + h, c);
+        line(x1 + w, y1 + h, x1,     y1 + h, c);
+        line(x1,     y1 + h, x1,     y1,     c);
+    }
+
+    void input() const {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                this->close();
+            }
+        }
+    }
+
+    void render(const unsigned int ms) const {
+        SDL_RenderPresent(renderer);
+        SDL_Delay(ms);
+    }
+};
+
+/*
+old line() code because i dont want to lose it:
+
+if (SCALE == 1) {
+    SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, c.a);
+    SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+    return;
+}
+int dx = x2 - x1;
+int dy = y2 - y1;
+float length = sqrt(dx * dx + dy * dy);
+
+for (int i = 0; i <= length; i++) {
+    int x = x1;
+    int y = y1;
+    if (length > 0) {
+        x += i * dx / length;
+        y += i * dy / length;
+    }
+    pixel(x, y, c);
+}
+*/
---
+++ mod2mp3.cpp
@@ -0,0 +1,61 @@
+/* not sure will this run on windows
+ *
+ * you can actually use any audio format not just mp3 i had no idea how to call it
+ *
+ * compiles with:
+ * g++ mod2mp3.cpp -o mod2mp3 -O3 -lopenmpt
+ *
+ * usage: ./mod2mp3 <input.(xm/mod/stm/s3m/it/etc.)> <output.(mp3/wav/ogg/etc.)>
+ */
+
+#include <bits/stdc++.h>
+#include <libopenmpt/libopenmpt.hpp>
+
+int main(int argc, char* argv[]) {
+    constexpr size_t buffersize = 480;
+    constexpr int32_t samplerate = 48000;
+    if (argc < 3) {
+        std::cout << "nouuuuuuuuu\n";
+        return 1;
+    }
+    std::ifstream file(argv[1], std::ios::binary);
+    openmpt::module mod(file);
+
+    std::string command = "ffmpeg -f s16le -ar " + std::to_string(samplerate) + " -ac 2 -i pipe: ";
+    for (char* c = argv[2]; *c; c++) {
+        if (*c == ' ') command += '\\';
+        command += *c;
+    }
+    std::cout << command << '\n';
+    FILE* pipe = popen(command.c_str(), "w");
+    if (!pipe) {
+        std::cerr << "Error: failed to open pipe\n";
+        return 1;
+    }
+
+    mod.set_render_param(openmpt::module::render_param::RENDER_INTERPOLATIONFILTER_LENGTH, 1); // no interpolation
+    mod.set_render_param(openmpt::module::render_param::RENDER_VOLUMERAMPING_STRENGTH, 0); // no volume ramping
+
+    try {
+        int16_t pcm_buffer_left[buffersize];
+        int16_t pcm_buffer_right[buffersize];
+        int16_t interleaved_buffer[buffersize * 2];
+        while (true) {
+            std::size_t count = mod.read(samplerate, buffersize, pcm_buffer_left, pcm_buffer_right);
+            if (count == 0) break;
+            for (size_t i = 0; i < count; ++i) {
+                interleaved_buffer[2 * i] = pcm_buffer_left[i];
+                interleaved_buffer[2 * i + 1] = pcm_buffer_right[i];
+            }
+            fwrite(interleaved_buffer, sizeof(int16_t), count * 2, pipe);
+            fflush(stdout);
+        }
+        pclose(pipe);
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << std::string(e.what() ? e.what() : "unknown error") << '\n';
+        return 1;
+    }
+
+    return 0;
+}
